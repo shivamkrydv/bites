6014e560be907cc29d40e2e84df792f93d5d8b38
Shivam kumar yadav <Shivamy7850@gmail.com>
2025-02-24 19:39:43 +0530

    before footer

---
+++ script.js
@@ -0,0 +1,15 @@
+let showsidenav = document.querySelector(".mainhambar");
+let hidesidenav = document.querySelector(".sidenavclose");
+
+let sidenav = document.querySelector(".sidenav");
+
+
+const showside = () =>{
+    sidenav.style.display = "flex";
+}
+const hideside = () =>{
+    sidenav.style.display = "none"
+}
+
+showsidenav.addEventListener("click",showside);
+hidesidenav.addEventListener("click",hideside);
